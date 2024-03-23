bd7f813879887bd2808db5ab5e797950d8bbfbf3
Shaswat Acharya <shaswatacharya123@gmail.com>
2024-03-23 13:23:29 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let userInput = document.getElementById("date");
+
 userInput.max = new Date().toISOString().split("T")[0];
+
 let result = document.getElementById("result");
 
 function calculateAge(){
+    
     let birthDate = new Date(userInput.value);
 
     let d1 = birthDate.getDate();
+    
     let m1 = birthDate.getMonth() + 1;
+    
     let y1 = birthDate.getFullYear();
 
     let today = new Date();
 
     let d2 = today.getDate();
+    
     let m2 = today.getMonth() + 1;
+    
     let y2 = today.getFullYear();
 
     let d3, m3, y3;
@@ -41,6 +48,9 @@ function calculateAge(){
     }
 
     result.innerHTML = `You are <span>${y3}</span> years, <span>${m3}</span> months and <span>${d3}</span> days old!`;
+
+
+    
 
 
 }
